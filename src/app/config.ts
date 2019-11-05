2c0434354114af3e4ac445c6ec6b005ea7e93236
bhavik103 <bhavikkalariya103@gmail.com>
2019-11-05 11:31:25 +0530

    pushed V0.0.4 to development

--- src/app/config.ts
+++ src/app/config.ts
@@ -11,7 +11,5 @@ export const config = {
 // 1) keytool -genkey -v -keystore my-release-key.keystore -alias alias_name -keyalg RSA -keysize 2048 -validity 10000
 // 2) jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore my-release-key.keystore platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk alias_name
 // 3) zipalign -v 4 platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk triviapost.apk
-
-
 //Google analytics error soln
 // ionic cordova plugin add cordova-android-play-services-gradle-release
